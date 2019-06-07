83dd7bfadcff5060032bf919247afb453d68cc6a
kevin <undyingmind@gmail.com>
2019-06-07 11:39:16 +0700

    Added export for commonjs

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ],
     plugins: [
       '@babel/plugin-syntax-dynamic-import',
       '@babel/plugin-proposal-class-properties'
